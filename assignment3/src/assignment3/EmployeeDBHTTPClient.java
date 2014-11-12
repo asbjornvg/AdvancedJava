c2577e0a51ded0d2a670d5776e2c7152c6b41d48
Jeppe <jeppe@ubuntu.(none)>
2014-11-12 13:13:11 +0100

    Added work on SimpleEmployeeDB and EmployeeDBHTTPClient

--- assignment3/src/assignment3/EmployeeDBHTTPClient.java
+++ assignment3/src/assignment3/EmployeeDBHTTPClient.java
@@ -1,10 +1,18 @@
 package assignment3;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.jetty.client.ContentExchange;
 import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.io.Buffer;
+import org.eclipse.jetty.io.ByteArrayBuffer;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
 
 /**
  * EmployeeDBHTTPClient implements the client side methods of EmployeeDB
@@ -18,6 +26,7 @@ public class EmployeeDBHTTPClient implements EmployeeDBClient, EmployeeDB {
 	private HttpClient client = null;
 	private static final String filePath = "/home/bonii/advanced-java/src/assignment3/departmentservermapping.xml";
 	private Map<Integer, String> departmentToServerURLMap = null;
+	private XStream xmlStream = new XStream(new StaxDriver());
 
 	public EmployeeDBHTTPClient() throws FileNotFoundException {
 		//Returns a concurrent hashmap :-)
@@ -28,12 +37,43 @@ public class EmployeeDBHTTPClient implements EmployeeDBClient, EmployeeDB {
 
 	@Override
 	public void addEmployee(Employee emp) throws DepartmentNotFoundException {
-		// TODO Auto-generated method stub
+		String xmlString = xmlStream.toXML(emp);
+		
+		ContentExchange exchange = new ContentExchange();
+		exchange.setMethod("POST");
+		exchange.setURL(getServerURLForDepartment(emp.getDepartment()));
+		Buffer buffer = new ByteArrayBuffer(xmlString);
+		exchange.setRequestContent(buffer);
+		try {
+			client.send(exchange);
+			exchange.waitForDone();
+			exchange.getResponseContent();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public List<Employee> listEmployeesInDept(List<Integer> departmentIds) {
-		// TODO Auto-generated method stub
+		for (Integer depId : departmentIds) {
+			ContentExchange exchange = new ContentExchange();
+			exchange.setMethod("POST");
+			try {
+				exchange.setURL(getServerURLForDepartment(depId));
+				client.send(exchange);
+				exchange.waitForDone();
+				exchange.getResponseContent();
+			} catch (DepartmentNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
 		return null;
 	}
 
---
+++ assignment3/src/assignment3/SimpleEmployeeDB.java
@@ -0,0 +1,63 @@
+	package assignment3;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+public class SimpleEmployeeDB implements EmployeeDB {
+
+	private static Hashtable<Integer, List<Employee>> employees = new Hashtable<Integer, List<Employee>>();
+	
+	public SimpleEmployeeDB(List<Integer> departmentIds) {
+		for (Integer depId : departmentIds) {
+			employees.put(depId, new ArrayList<Employee>());
+		}
+
+	}
+
+	@Override
+	public synchronized void addEmployee(Employee emp)
+			throws DepartmentNotFoundException {
+		if(!employees.containsKey(emp.getDepartment())){
+			throw new DepartmentNotFoundException("Department not found!");
+		}
+		else{ 
+			employees.get(emp.getDepartment()).add(emp);
+		}
+	}
+
+	@Override
+	public synchronized List<Employee> listEmployeesInDept(List<Integer> departmentIds) {
+		List<Employee> result = new ArrayList<Employee>();
+		for (Integer depId : departmentIds) {
+			result.addAll(employees.get(depId));
+		}
+		return result;
+	}
+
+	@Override
+	public synchronized void cleanupDB() {
+		employees.clear();
+	}
+
+	@Override
+	public synchronized void incrementSalaryOfDepartment(
+			List<SalaryIncrement> salaryIncrements)
+			throws DepartmentNotFoundException,
+			NegativeSalaryIncrementException {
+		for (SalaryIncrement salaryIncrement : salaryIncrements) {
+			if(salaryIncrement.getIncrementBy() < 0){
+				throw new NegativeSalaryIncrementException("Found a negative salary!");
+			}
+			if(!employees.containsKey(salaryIncrement.getDepartment())){
+				throw new DepartmentNotFoundException("Department not found!");
+			}		
+		}
+		for (SalaryIncrement salaryIncrement : salaryIncrements) {
+			float increaseAmount = salaryIncrement.getIncrementBy();
+			for (Employee emp : employees.get(salaryIncrement.getDepartment())) {
+				emp.setSalary(emp.getSalary() + increaseAmount);
+			}
+		}	
+	}
+}
